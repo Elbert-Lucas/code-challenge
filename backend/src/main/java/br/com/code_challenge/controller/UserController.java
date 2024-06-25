152ed6ccda488ba947ba84a6d766da4b8b9de12e
Elbert-Lucas <63523828+Elbert-Lucas@users.noreply.github.com>
2024-06-25 00:43:38 -0300

    Logs, Handler e Mensagens

--- backend/src/main/java/br/com/code_challenge/controller/UserController.java
+++ backend/src/main/java/br/com/code_challenge/controller/UserController.java
@@ -4,6 +4,7 @@ import br.com.code_challenge.dto.request.RegisterUserDTO;
 import br.com.code_challenge.dto.response.abstracts.MessageResponseDTO;
 import br.com.code_challenge.service.UserService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -26,6 +28,7 @@ public class UserController {
 
     @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<MessageResponseDTO> registerUser(@RequestBody @Valid RegisterUserDTO registerUserDTO){
+        log.debug("Iniciando registro de usuario");
         MessageResponseDTO response = userService.registerUser(registerUserDTO);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }

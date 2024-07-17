0f122969968d86d05b6977cfc6257d4c2a98ed66
Elbert-Lucas <63523828+Elbert-Lucas@users.noreply.github.com>
2024-07-17 03:07:29 -0300

    Primeiros testes unitarios (user)

--- backend/src/test/java/br/com/code_challenge/unit/service/UserServiceUT.java
+++ backend/src/test/java/br/com/code_challenge/unit/service/UserServiceUT.java
@@ -1,19 +1,14 @@
 package br.com.code_challenge.unit.service;
 
 import br.com.code_challenge.domain.Address;
-import br.com.code_challenge.domain.User;
-import br.com.code_challenge.dto.request.RegisterUserDTO;
-import br.com.code_challenge.repository.RoleRepository;
+import br.com.code_challenge.dto.request.RegisterUserDto;
 import br.com.code_challenge.repository.UserRepository;
 import br.com.code_challenge.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.env.Environment;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
@@ -22,7 +17,6 @@ import org.springframework.test.context.ActiveProfiles;
 import java.io.File;
 import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -34,35 +28,35 @@ public class UserServiceUT {
     @Autowired
     UserRepository userRepository;
 
-    static RegisterUserDTO registerUserDTO;
+    static RegisterUserDto registerUserDto;
 
     @BeforeAll
     static void setup() throws IOException {
         String modelsPath = "src/test/resources/models/";
         String registerUserPath = modelsPath + "user-register.json";
-        registerUserDTO =  new ObjectMapper().readValue(new File(registerUserPath), RegisterUserDTO.class);
+        registerUserDto =  new ObjectMapper().readValue(new File(registerUserPath), RegisterUserDto.class);
     }
     @Autowired
     private Environment environment;
 
     @Test
     void shouldRegisterUserSuccessfully(){
-        assertEquals(HttpStatus.CREATED, userService.registerUser(registerUserDTO).getStatus());
+        assertEquals(HttpStatus.CREATED, userService.registerUser(registerUserDto).getStatus());
     }
 
     @Test
     void shouldThrowExceptionOnDuplication() throws IOException {
         String modelsPath = "src/test/resources/models/";
         String registerUserPath = modelsPath + "user-register.json";
-        RegisterUserDTO duplicateDto =  new ObjectMapper().readValue(new File(registerUserPath), RegisterUserDTO.class);
+        RegisterUserDto duplicateDto =  new ObjectMapper().readValue(new File(registerUserPath), RegisterUserDto.class);
         assertThrows(DataIntegrityViolationException.class, () -> userService.registerUser(duplicateDto));
     }
 
     @Test
     void shouldThrowExceptionOnInvalidData(){
-        registerUserDTO.setAddress(new Address());
-        registerUserDTO.setEmail("Test@test.com");
-        registerUserDTO.setPhone("999999999999999999999");
-        assertThrows(DataIntegrityViolationException.class, () -> userService.registerUser(registerUserDTO));
+        registerUserDto.setAddress(new Address());
+        registerUserDto.setEmail("Test@test.com");
+        registerUserDto.setPhone("999999999999999999999");
+        assertThrows(DataIntegrityViolationException.class, () -> userService.registerUser(registerUserDto));
     }
 }
